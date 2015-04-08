6bdbba67a2dec88d1968d377568d732e4fbbb662
kurtengsig <kurt.engsig@hotmail.com>
2015-04-08 13:08:54 -0400

    Includes legend and changed points to a bar graph

--- ComparisonPanel.java
+++ ComparisonPanel.java
@@ -31,6 +31,7 @@ public class ComparisonPanel extends JPanel{
 		g.setColor(Color.black);
 		g.drawLine(0, 0, 900, 0);
 		drawGridLines(g, 10, 8);
+		drawLegend(g);
 		plotPoints(g, 11, 8);
 		drawAxis(g, "Channels In Use", "2.4 GHz Channels", "Frequency of Use");
 		getSuggestedAP();
@@ -40,13 +41,13 @@ public class ComparisonPanel extends JPanel{
 		
 		//Draws the x and y axis
 		g2.drawLine(40, 40, 40, height-40);
-		g2.drawLine(40, height-40, 880-40, height-40);
+		g2.drawLine(40, height-40, 880-48, height-40);
 		
 		//Draws the title
 		g2.drawString(title, 400, 30);
 		
 		//Labels the x axis
-		g2.drawString(xAxis, 400, height-10);
+		g2.drawString(xAxis, 400, height-15);
 		
 		//Rotates the context to write the label for the y axis
 		AffineTransform transform = new AffineTransform();
@@ -57,19 +58,36 @@ public class ComparisonPanel extends JPanel{
 		transform.rotate(Math.PI/2); // returns the context to normal
 	}
 	public void drawGridLines(Graphics g, int x, int y){
-		int xIncrement = (800)/x;
+		int xIncrement = (800)/(x+1);
 		int yIncrement = (height-80)/y;
 		g.setColor(Color.LIGHT_GRAY);
 		for(int i=0; i<y; i++){
-			g.drawLine(40, 40+(i*yIncrement), 840, 40+(i*yIncrement));
+			g.drawLine(40, 40+(i*yIncrement), 832, 40+(i*yIncrement)); // draw y gridlines
+			g.setColor(Color.BLACK);
+			g.drawString(""+(8-i), 30, 44+(i*yIncrement));
+			g.setColor(Color.LIGHT_GRAY);
 		}
-		for(int i=0; i<x; i++){
-			g.drawLine(40+xIncrement+(i*xIncrement), 40, 40+xIncrement+(i*xIncrement), height-40);
+		for(int i=0; i<x+1; i++){
+			g.drawLine(40+xIncrement+(i*xIncrement), 40, 40+xIncrement+(i*xIncrement), height-40); //draw x gridlines
 		}
 		g.setColor(Color.black);
 	}
+	public void drawLegend(Graphics g){
+		g.setColor(Color.WHITE);
+		g.fillRect(670, 44, 160, 40);
+		g.setColor(Color.BLACK);
+		g.drawRect(670, 44, 160, 40);
+		g.setColor(Color.BLUE);
+		g.fillRect(675, 50, 7, 7);
+		g.setColor(Color.BLACK);
+		g.drawString("Suggested AP Channel", 688, 58);
+		g.setColor(Color.BLACK);
+		g.drawString("Suggested Access Point", 688, 78);
+		g.setColor(Color.RED);
+		g.fillRect(675, 70, 7, 7);
+	}
 	public void plotPoints(Graphics g, int x, int y){
-		int xIncrement    = (800)/(x-1);
+		int xIncrement    = (800)/(x);
 		int yIncrement    = (height-80)/y;
 		int currentYCount = 0;
 		int currentX      = 0;
@@ -84,35 +102,27 @@ public class ComparisonPanel extends JPanel{
 				}
 			}
 			plotted.add(new PlottedPoints(currentX, currentYCount));
-			//System.out.println("Plotting for points: "+currentX+","+currentYCount);
-			g.fillOval(40-3+(xIncrement*currentX), (height-(37+ (yIncrement*currentYCount))-10), 6, 6);
-			if(currentX == suggestion){
-				g.setColor(Color.BLUE);
-				g.drawOval(40-5+(xIncrement*currentX), (height-(39+ (yIncrement*currentYCount))-10), 10, 10);
-				g.drawOval(40-6+(xIncrement*currentX), (height-(40+ (yIncrement*currentYCount))-10), 12, 12);
-				g.drawOval(40-7+(xIncrement*currentX), (height-(41+ (yIncrement*currentYCount))-10), 14, 14);
-				
-				g.setColor(Color.BLACK);
-			}
+			//g.fillOval(40-3+(xIncrement*currentX), (height-(37+ (yIncrement*currentYCount))-10), 6, 6);
 			if(currentX == suggestedAp-1){
 				g.setColor(Color.RED);
-				g.drawOval(40-5+(xIncrement*currentX), (height-(39+ (yIncrement*currentYCount))-10), 10, 10);
-				g.drawOval(40-6+(xIncrement*currentX), (height-(40+ (yIncrement*currentYCount))-10), 12, 12);
-				g.drawOval(40-7+(xIncrement*currentX), (height-(41+ (yIncrement*currentYCount))-10), 14, 14);
-				
-				g.setColor(Color.BLACK);
 			}
+			if(currentX == suggestion){
+				g.setColor(Color.BLUE);
+			}
+			g.fillRect(60-3+(xIncrement*currentX), (height-(yIncrement*currentYCount)-45), 40, 5+(yIncrement*currentYCount));
+			g.setColor(Color.BLACK);
+			
 			currentX +=1;
 			currentYCount = 0;
 		}
 		PlottedPoints prevPoint = plotted.get(0);
 		for(int i=1; i<plotted.size(); i++){
-			g.drawString(""+i, 36+(xIncrement*prevPoint.x), height - 25);
-			g.drawLine(40+(xIncrement*prevPoint.x), (height-(37+ (yIncrement*prevPoint.y+3))-4), 
-					40+(xIncrement*plotted.get(i).x), (height-(37+ (yIncrement*plotted.get(i).y+3)))-4);
+			g.drawString(""+i, (xIncrement*(prevPoint.x+1)), height - 25);
+			//g.drawLine(40+(xIncrement*prevPoint.x), (height-(37+ (yIncrement*prevPoint.y+3))-4), 
+				//	40+(xIncrement*plotted.get(i).x), (height-(37+ (yIncrement*plotted.get(i).y+3)))-4);
 			prevPoint = plotted.get(i);
 		}
-		g.drawString(""+x, 36+(xIncrement*(x-1)), height - 25);
+		g.drawString(""+x, (xIncrement*(x)), height - 25);
 		g.setColor(Color.black);
 	}
 	public int getChannelScore(int channel){
